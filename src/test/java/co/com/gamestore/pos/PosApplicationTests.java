d0c14a5c6db55ec3bd8de46e4fe6d9181cc17065
Jonathan Barrera <andres.barar@gmail.com>
2020-10-10 22:03:31 -0500

    initial commit

---
+++ src/test/java/co/com/gamestore/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.gamestore.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
